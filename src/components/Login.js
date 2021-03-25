dceeb08c29e9ceccb16b3513f125dd111fa0ac1d
jomapormentilla <jomapormentilla@gmail.com>
2021-03-25 15:50:25 -0400

    Disable Google OAuth (temporarily). Update favicon.

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,18 +23,18 @@ class Login extends React.Component {
     render(){
         return(
             <div className="login">
-                <form onSubmit={ this.handleOnSubmit } style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', width: '350px', alignItems: 'center' }}>
+                <form onSubmit={ this.handleOnSubmit } style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', width: '350px', alignItems: 'center', borderBottom: 'solid 1px #fff', marginBottom: '15px', paddingBottom: '15px' }}>
                     <h1 className="logo" style={{ fontSize: '80px' }}>Jomatrix</h1>
                     <input type="email" name="email" value={ this.state.email } placeholder="Email" onChange={ this.handleOnChange } />
                     <input type="password" name="password" value={ this.state.password } placeholder="Password" onChange={ this.handleOnChange } />
                     <button type="submit">Login</button>
                 </form>
 
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: '#fff', width: '350px', borderTop: 'solid 1px #fff', marginTop: '15px', padding: '15px 0px', borderBottom: 'solid 1px #fff', marginBottom: '15px' }}>
+                {/* <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: '#fff', width: '350px', borderTop: 'solid 1px #fff', marginTop: '15px', padding: '15px 0px', borderBottom: 'solid 1px #fff', marginBottom: '15px' }}>
                     <button className="loginGoogle">
                         Login with Google
                     </button>
-                </div>
+                </div> */}
 
                 <div className="signupLink" style={{ marginBottom: '200px' }}>
                     <NavLink to="/signup">Sign up for an account</NavLink>
