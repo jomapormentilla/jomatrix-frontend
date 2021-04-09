0db4c08745d91d7e4a496970b6bbcacc6fe6a73c
jomapormentilla <jomapormentilla@gmail.com>
2021-04-09 10:56:31 -0400

    Uninstall react-dotenv cont.

--- create-env.js
+++
@@ -1,2 +0,0 @@
-const fs = require('fs')
-fs.writeFileSync('./.env', `AWS_ACCESS_KEY_ID=${process.env.AWS_ACCESS_KEY_ID_NETLIFY}\nAWS_SECRET_ACCESS_KEY=${process.env.AWS_SECRET_ACCESS_KEY_NETLIFY}`)
