dc7a01602d8f6c8f97ee1fc74b54ec90da52d245
jomapormentilla <jomapormentilla@gmail.com>
2021-04-09 12:10:49 -0400

    Update base url. Connect to new backend api.

--- src/actions/baseUrl.js
+++ src/actions/baseUrl.js
@@ -1,3 +1,3 @@
-export const url = `https://powerful-brushlands-01810.herokuapp.com`
-
+export const url = `https://jomatrix-backend.herokuapp.com`
 // export const url = `http://localhost:3000`
+// export const url = `https://powerful-brushlands-01810.herokuapp.com`
