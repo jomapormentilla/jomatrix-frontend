8e4af363f76d9aab2513e956f2c64010278d6ca4
jomapormentilla <jomapormentilla@gmail.com>
2021-07-06 18:45:09 -0400

    Fix href bug.

--- src/components/profile/ProfileNav.js
+++ src/components/profile/ProfileNav.js
@@ -17,7 +17,7 @@ const ProfileNav = props => {
             <ul>
                 <Link to={`${url}/edit`}><li>Edit Profile</li></Link>
                 <Link to={`${url}/image`}><li>Change Image</li></Link>
-                { props.currentUser.id === 16 ? <a href="javascript:void(0)"><li style={{ textDecoration: 'line-through' }}>Change Password</li></a> : <Link to={`${url}/password`}><li>Change Password</li></Link> }
+                { props.currentUser.id === 16 ? <Link to={`${url}/edit`}><li style={{ textDecoration: 'line-through' }}>Change Password</li></Link> : <Link to={`${url}/password`}><li>Change Password</li></Link> }
             </ul>
         </div>
     )
